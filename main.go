78bbe6a79aee8a024d92481bad4f953878bccd10
Lucasdip <lucaslimadip@gmail.com>
2026-02-20 16:02:21 -0300

    adição do front-end

--- main.go
+++ main.go
@@ -21,9 +21,22 @@ func main() {
 
 
     r := gin.Default()
+	
+	r.Use(func(c *gin.Context) {
+    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+    c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    
+    if c.Request.Method == "OPTIONS" {
+        c.AbortWithStatus(204)
+        return
+    }
+    c.Next()
+})
 
    
 	r.POST("/login", handlers.Login)
+
 
 	// Criamos um grupo protegido
 	authorized := r.Group("/tasks")
